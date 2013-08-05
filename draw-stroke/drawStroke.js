2df9c55e9354519a3aa9ca3a9462db799bd28b2f
Sosuke Masui <esmasui@gmail.com>
2013-08-05 15:45:42 +0900

    colorの形式をrgbaだと誤解していたが正しくはargbだったので、colorのパース処理を修正した

--- draw-stroke/drawStroke.js
+++ draw-stroke/drawStroke.js
@@ -1,32 +1,33 @@
-function RGBOf(color) {
-    var rgb = [];
+function parseColor(color) {
+    var rgba = [];
     for(var c = 2; c >= 0; --c)
-        rgb.push((color >> (c * 8)) & 0xFF);
-    return rgb;
-};
-
-function RGBAOf(rgb, alpha) {
-    var rgba = rgb.slice();
-    rgba.push(parseInt(alpha, 10));
+        rgba.push((color >> (c * 8)) & 0xFF);
+    rgba.push((color >> 24) & 0xFF);
     return rgba;
 };
 
+function setAlpha(rgba, alpha) {
+    var c = rgba.slice();
+    c[3] = parseInt(c[3] * alpha, 10) & 0xFF;
+    return c;
+};
+
 function styleOf(color) {
-    return (color.length == 4 ? 'rgba' : 'rgb') + '(' + color.join(',') + ')';
+    return 'rgba(' + color.join(',') + ')';
 }
 
 var running = null;
 var tasks = [];
 var taskCount = 0;
 
-function drawStroke(stroke, ctx) {
+function drawStroke(info, stroke, ctx) {
     var lineWidth = stroke.width;
     var strokeData = stroke.data;
-    var strokeColor = RGBOf(stroke.color);
+    var strokeColor = parseColor(stroke.color);
     
     for(var j = 0, px =0, py = 0; j < strokeData.length; j+=3) {
-        var x = strokeData[j];
-        var y = strokeData[j+1];
+        var x = strokeData[j] + info.x;
+        var y = strokeData[j+1] + info.y;
         var p = strokeData[j+2];
 
         if(j == 0) {
@@ -37,7 +38,7 @@ function drawStroke(stroke, ctx) {
 
         tasks.push((function(ctx, strokeColor, lineWidth, px, py, x, y, p){
             return function(){
-                ctx.strokeStyle = styleOf(RGBAOf(strokeColor, 0xFF * p));
+                ctx.strokeStyle = styleOf(setAlpha(strokeColor, p));
                 ctx.lineWidth = parseInt(lineWidth * p, 10);
 
                 ctx.beginPath();
@@ -54,12 +55,12 @@ function drawStroke(stroke, ctx) {
 
 function drawStrokes(info, canvas) {
     var strokes = info.strokes;
-    canvas.width = info.width;
-    canvas.height = info.height;
+    // canvas.width = info.width;
+    // canvas.height = info.height;
     var ctx = canvas.getContext('2d');
-    ctx.fillStyle = styleOf(RGBOf(info.color));
-    ctx.fillRect(0, 0, info.width, info.height);
-    strokes.forEach(function(e){drawStroke(e, ctx)});
+    ctx.fillStyle = styleOf(info.color != 0 ? parseColor(info.color) : [0x00,0x00,0x00,0xFF]);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    strokes.forEach(function(e){drawStroke(info, e, ctx)});
     taskCount = tasks.length;
 
     running = setInterval(function(){
